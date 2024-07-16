f334a72ccd4332a7bf0c9bb98f8c756e3f2a4a57
Siva Durai <banuselvi1510@gmail.com>
2024-07-16 15:59:15 +0530

    First Time Push

---
+++ src/test/java/org/story/WebTesting.java
@@ -0,0 +1,28 @@
+package org.story;
+
+public class WebTesting {
+
+	private void landTrans() {
+		System.out.println("Bus"); 	
+	}
+	private void seaTrans() {
+		System.out.println("Ship");
+	}
+	private void airTrans(){
+		System.out.println("Aero");
+	}
+	private void india(){
+		System.out.println("ind");
+	}
+	private void USA(){
+		System.out.println("Us");
+	}
+
+	public static void main(String[] args) {
+		WebTesting f=new WebTesting();
+		f.landTrans();
+		f.airTrans();
+		f.seaTrans();
+	}
+	}
+
